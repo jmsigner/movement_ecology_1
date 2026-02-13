a5c420241ac01b28664c454cc3394eafc5337e62
Johannes Signer <jmsigner@gmail.com>
2026-02-13 23:19:44 +0100

    mod 7

--- required_packages.R
+++ required_packages.R
@@ -1,27 +1,27 @@
 required.packages <- c(
-  "amt", 
-  "tidyverse", 
-  "lubridate", 
-  "moveHMM", 
+  "amt",
+  "tidyverse",
+  "lubridate",
+  "moveHMM",
   "CircStats",
-  "sf", 
-  "here", 
-  "raster", 
-  "terra",
-  "move", 
-  "crawl", 
+  "sf",
   "here",
-  "glmmTMB", 
-  "ctmm", 
+  "raster",
+  "terra",
+  "move",
+  "crawl",
+  "here",
+  "glmmTMB",
+  "ctmm",
   "conflicted",
   "mvtnorm",
   "scales",
   "rcompanion",
   "pROC",
-  "wrswoR", 
-  "ResourceSelection", 
-  "terra", 
-  "suncalc", 
+  "wrswoR",
+  "ResourceSelection",
+  "terra",
+  "suncalc",
   "remotes"
 )
 
@@ -46,6 +46,6 @@ install.packages(all.pkgs[!all.pkgs %in% row.names(installed.packages())])
 # Make sure you have the latest version of all packages.
 update.packages(ask = FALSE)
 
-# Add amt development version
+
 remotes::install_github("jmsigner/amt", upgrade = "never")
 
